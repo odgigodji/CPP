6653cbf6f964566b21eebdb12c9de986d762f605
odgigodji <AZM-snn-J9f-G2L>
2021-07-04 15:06:34 +0300

    hello i'm Karen

---
+++ CPP01/ex05/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Karen.hpp"
+
+int main() {
+	Karen bot;
+	bot.complain("DEBUG");
+//	bot.complain("INFO");
+//	bot.complain("WARNING");
+//	bot.complain("ERROR");
+//	bot.complain("not valid command");
+	return (0);
+}
