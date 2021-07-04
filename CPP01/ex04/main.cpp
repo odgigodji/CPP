9ebdb6c070112d04362a8690c86480d81523545a
odgigodji <AZM-snn-J9f-G2L>
2021-07-04 13:43:59 +0300

    somewhat work

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -4,38 +4,44 @@
 # define RESET  "\x1B[0m"
 
 void error(int errCode) {
+	if (errCode == 3)
+		std::cout << CYN"Error: empty file\n"RESET;
 	exit(errCode);
 }
 
-void readFile(char *arg, std::string &strFile) {
+void writeFile(std::string &strFile, std::string filename) {
+
+	std::ofstream file(filename.append(".replace"));
+	if (!file)
+		error(4);
+	file << strFile;
+}
+
+void readFile(std::string &strFile, char *filename) {
 	char c;
 
-	std::ifstream file(arg); //поток для чтения из файла типо open argv имя файла
-	if (!file)
+	std::ifstream fileIn(filename); 	//поток для чтения из файла
+	if (!fileIn)
 		error(2);
-	while ((c = file.get()) != EOF) { //whole file in string
-		strFile.push_back(c); //добавить символ в конец
+	while ((c = fileIn.get()) != EOF) { //whole file in string
+		strFile.push_back(c); 			//добавить символ в конец
 	}
-	file.close();
+	fileIn.close();
 	if (strFile.length() == 0)
 		error(3);
 }
 
 void replaceStrings(std::string &strFile, const std::string& s1, const std::string& s2)
 {
-//	int i = 0;
-//	while ((i = strFile.find(s1, i)) != -1)
-	std::string cyn = CYN;
-	std::string reset = RESET;
 	for (int i = 0; (i = strFile.find(s1, i)) != -1;) //ищет начало вхождения и возвршает индекс вхождения
 	{
-		strFile.erase(i, s1.length());  //чистит с какого и сколлько чистить
-		strFile.insert(i, cyn);
-		i += cyn.length();
-		strFile.insert(i, s2);			//вставляем с какого какую строку
-		i += s2.length();				//промати=ываем индекс на длину вставленной строки
-		strFile.insert(i, reset);
+		strFile.erase(i, s1.length());  //чистит с какого индекса и сколлько чистить
+		strFile.insert(i, CYN);			//вставляем с какого индекса какую строку
+		i += strlen(CYN);				//проматиываем индекс на длину вставленной строки
+		strFile.insert(i, s2);
 		i += s2.length();
+		strFile.insert(i, RESET);
+		i += strlen(RESET);
 	}
 }
 
@@ -43,15 +49,13 @@ int main(int ac, char** av)
 {
 	//reading file in string
 	std::string strFile;
-	readFile(av[1], strFile);
-	std::cout << strFile;
-	//replace strings in filestring
+	readFile(strFile, av[1]);
+
+	//replace strings in strFile
 	replaceStrings(strFile, av[2], av[3]);
+
 	//write string to file
-	std::cout << strFile;
-	//check error if occurs
+	writeFile(strFile, av[1]);
 
-
-//
-//	return 0;
+	return 0;
 }
