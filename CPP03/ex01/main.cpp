9fb44abd46f8aa95934406262bef2a226a3e68ff
odgigodji <AZM-snn-J9f-G2L>
2021-07-08 20:56:10 +0300

    first commit

---
+++ CPP03/ex01/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "ClapTrap.hpp"
+
+int main() {
+	ScavTrap sc("ScavBoy");
+	std::cout << sc << std::endl;
+
+	sc.attack("somewhat");
+	sc.takeDamage(15);
+	std::cout << sc << std::endl;
+
+	sc.beRepaired(10);
+	std::cout << sc << std::endl;
+
+	sc.guardGate();
+
+	ScavTrap a;
+	a = sc;
+	a.setName("Aleks");
+	std::cout << a << std::endl;
+	return 0;
+}
