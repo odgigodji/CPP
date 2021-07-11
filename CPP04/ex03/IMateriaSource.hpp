379e47007824aa6509af4fd1329985edd73edd2f
odgigodji <AZM-snn-J9f-G2L>
2021-07-11 21:26:58 +0300

    starting work

--- CPP04/ex03/IMateriaSource.hpp
+++ CPP04/ex03/IMateriaSource.hpp
@@ -10,7 +10,15 @@ class IMateriaSource
 {
 public:
 	virtual ~IMateriaSource() {}
+
+//	learnMateria must copy the Materia passed as a parameter, and store it in memory
+//	to be cloned later. Much in the same way as for Character , the Source can know at
+//			most 4 Materia, which are not necessarily unique.
 	virtual void learnMateria(AMateria*) = 0;
+
+//	createMateria(std::string const &) will return a new Materia, which will be a
+//	copy of the Materia (previously learned by the Source) which type equals the parameter.
+//	Returns 0 if the type is unknown.
 	virtual AMateria* createMateria(std::string const & type) = 0;
 };
 
--- CPP04/ex03/MateriaSource.cpp
+++ CPP04/ex03/MateriaSource.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "MateriaSource.hpp"
+
+MateriaSource::MateriaSource() {
+
+}
+
+MateriaSource::MateriaSource(std::string &name) {
+
+}
+
+MateriaSource::MateriaSource(MateriaSource &i) {
+
+}
+
+MateriaSource &MateriaSource::operator=(MateriaSource &rhs) {
+	return *this;
+}
+
+MateriaSource::~MateriaSource() {
+
+}
