1a3f4607c2276f034a2856c07053a70c7c3a371b
odgigodji <santa51@mail.ru>
2021-07-17 17:33:13 +0300

    //instr add

--- CPP05/ex00/main.cpp
+++ CPP05/ex00/main.cpp
@@ -1,25 +1,24 @@
 #include <iostream>
-#include <stdexcept>
 #include "Bureaucrat.hpp"
 
-
-
 int main() {
+	//call a constructor with incorrect grade
 	Bureaucrat beth("Beth", 1);
 	std::cout << std::endl;
 
+	//call constructor with correct args and downGrade
 	Bureaucrat rick("Rick", 149);
 	rick.downGrade();
 	std::cout << rick << std::endl;
 	rick.downGrade();
 	std::cout << rick << std::endl << std::endl;
 
+	//call constructor with correct args and upGrade
 	Bureaucrat morty("Morty", 2);
 	morty.upGrade();
 	std::cout << morty << std::endl;
 	morty.upGrade();
 	std::cout << morty << std::endl;
-
 
 	return 0;
 }
