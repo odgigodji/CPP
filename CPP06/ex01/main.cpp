3bc6c3466ff71d844dbe356d4b0adbc5d63980cc
odgigodji <santa51@mail.ru>
2021-07-24 17:33:14 +0300

    working +-

--- CPP06/ex01/main.cpp
+++ CPP06/ex01/main.cpp
@@ -3,53 +3,43 @@
 //#include <cstdint>
 
 struct Data {
-	std::string type;
+	std::string name;
 	int         age;
 };
 
-
-//uinptr_t numeric value of a pointers
 uintptr_t serialize(Data* ptr) {
-	std::cout << "first pointer is " << ptr << std::endl;
-
 	void *tmp;
 	uintptr_t res;
+
+	std::cout << "first pointer is " << ptr << std::endl;
 	tmp = static_cast<void *>(ptr);
-
 	res = (uintptr_t )(tmp);
-
-//	uintptr_t i = reinterpret_cast<uintptr_t>(p);
-//	std::cout << ptr->type << std::endl;
-//	return static_cast<uintptr_t >(static_cast<void *>(ptr));
-//	std::cout << *res << std::endl;
-
 	return res;
 }
 
 Data* deserialize(uintptr_t raw) {
-	std::cout << raw << std::endl;
 	void *tmp;
 	Data *ptr;
 
 	tmp = (void *)raw;
-
 	ptr = static_cast<Data *>(tmp);
-//	uintptr_t *res = &raw;
-//
-//	tmp = static_cast<void *>(res);
-//	ptr = (Data *)(tmp);
 	std::cout << "last pointer is " << ptr << std::endl;
 	return ptr;
 }
 
 int main() {
 	Data *a = new Data;
-	a->type = "schoolar";
+	a->name = "Thor";
 	a->age  = 42;
+	std::cout << "Data a: type: \"" << a->name << "\" age: " << a->age << std::endl;
 
-	uintptr_t gg = serialize(a);
-//	std::cout << gg << std::endl;
+//call serialize
+	uintptr_t raw = serialize(a);
+	std::cout << "Raw is " << raw << std::endl;
 
-	deserialize(gg);
+//call deserialize
+	Data *b = new Data;
+	b = deserialize(raw);
+	std::cout << "Data b: type: \"" << b->name << "\" age: " << b->age << std::endl;
 	return 0;
 }
