60ae588051ae1284e6f1acf691aafbf5b0ecdaf2
odgigodji <santa51@mail.ru>
2021-07-29 17:41:14 +0300

    somethink wok 0.1

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -9,60 +9,146 @@
 # define RED    "\x1B[31m"
 # define RESET  "\x1B[0m"
 
-template <class T>
-class Array {
+//template <class T>
+//class Array {
+//public:
+//	T       *_arr;
+//	size_t  _size;
+//
+//public:
+//	Array() : _arr(), _size(0) {
+////		std::cout << "+Array() size: " << _size << " arr: " << _arr << std::endl;
+//	}
+//
+//	Array(unsigned int n) : _arr(), _size(n) {
+//		if (_size != 0) { _arr = new T[_size]; }
+////		std::cout << "+Array(n) size: " << _size << " _arr: ";
+////		std::cout << _arr << "_arr[0]: " << _arr[0] << std::endl;
+//	}
+//
+//	~Array() {
+//		if (_arr[0]) {
+////			std::cout << "-del call" << std::endl;
+//			delete[] _arr;
+//			_arr = NULL;
+//		}
+//
+//		std::cout << "~Array()" << std::endl;
+//	};
+//
+////	Array<T>(const Array<T> &origin) {
+////		*this = origin;
+//
+////		std::cout << "check1.9 \n";
+////		if (_arr[0]) {
+////			std::cout << "check2.0 \n";
+////			delete[] _arr;
+//////			_array = NULL;
+////			std::cout << _arr[1] << std::endl;
+////		}
+////		std::cout << "check2.1 \n";
+////		_size = origin._size;
+////		_arr = new T[_size];
+////		for (unsigned int i = 0; i < _size; ++i) {
+////			_arr[i] = origin._arr[i];
+////		}
+////	}
+//
+//	Array(const Array& copy)
+//	{
+//		this->_arr = new T[copy.size()]();
+//		this->_size = copy.size();
+//		for (unsigned int i = 0; i < this->size(); i++)
+//			this->_arr[i] = copy._arr[i];
+//	};
+////
+//
+////	Array &operator=(const Array& op)
+////	{
+////		std::cout << "=:" << std::endl;
+////		if (this == &op)
+////			return (*this);
+////		if (this->_arr[0]) {
+////			delete[] _arr;
+////			_arr = NULL;
+////		}
+////		this->_arr = new T[op.size()]();
+////		this->_size = op.size();
+////		for (int i = 0; i < this->size(); i++)
+////			this->_arr[i] = op._arr[i];
+////		return (*this);
+////	};
+//
+//
+//////MyClass<T>& operator+=(const MyClass<T>& classObj)
+//	Array<T> &operator=( Array<T>  &rhs) {
+//		std::cout << "operator =: \n";
+//		std::cout << "_size " << _size;
+//		std::cout << " _arr " << _arr << std::endl;
+//
+////		if (_arr) {
+////			std::cout << "del call " << std::endl;
+//			delete[] _arr;
+//////			_arr = NULL;
+////		}
+//		_size = rhs._size;
+//		_arr = new T[rhs._size];
+//		for (unsigned int i = 0; i < _size; ++i) {
+//			_arr[i] = rhs._arr[i];
+//		}
+//		return *this;
+//	}
+//
+
+
+//
+//	size_t size() const { return (_size); }
+//};
+
+
+template<class T>
+class Array
+{
 private:
 	T       *_arr;
 	size_t  _size;
 
 public:
-	Array() : _size(0), _arr(new T[0]) {
+	Array() : _arr(new T[0]()), _size(0) {
 		std::cout << "+Array() size: " << _size << " arr: " << _arr << std::endl;
-	}
+	} ;
+	Array(unsigned int n) : _arr(new T[n]()), _size(n) {} ;
 
-	Array(unsigned int n) :  _arr(new T[n]), _size(n) {
-		std::cout << "+Array(n) size: " << _size << " _arr: ";
-		std::cout << _arr << "_arr[0]: " << _arr[0] << std::endl;
-	}
-
-	~Array() {
-		delete[] _arr;
-		std::cout << "~Array()" << std::endl;
+	Array(const Array& origin)
+	{
+		_arr = new T[origin.size()]();
+		_size = origin.size();
+		for (int i = 0; i < size(); i++)
+			_arr[i] = origin._arr[i];
 	};
 
-	Array<T>(const Array<T> &origin) {
-		*this = origin;
-//		std::cout << "check1.9 \n";
-//		if (_arr[0]) {
-//			std::cout << "check2.0 \n";
-//			delete[] _arr;
-////			_array = NULL;
-//			std::cout << _arr[1] << std::endl;
-//		}
-//		std::cout << "check2.1 \n";
-//		_size = origin._size;
-//		_arr = new T[_size];
-//		for (unsigned int i = 0; i < _size; ++i) {
-//			_arr[i] = origin._arr[i];
-//		}
-	}
+	~Array() {
+		delete [] _arr;
+	};
 
-	Array<T> &operator=(const Array<T> &rhs) {
-		std::cout << "check2 \n";
-		std::cout << "_size " << _size << " _arr " << _arr << " _arr[0] " << _arr[0] << std::endl;
-		if (this == &rhs) {
-			return *this;
-		}
-		if (_arr[0]) {
-			delete[] _arr;
-////			_arr = NULL;
-		}
-		_size = rhs._size;
-		_arr = new T[rhs._size];
-		for (unsigned int i = 0; i < _size; ++i) {
-			_arr[i] = rhs._arr[i];
-		}
-		return *this;
+	Array &operator=(const Array& origin)
+	{
+		if (this == &origin)
+			return (*this);
+		if (this->_arr) { delete[] _arr; }
+		this->_arr = new T[origin.size()]();
+		this->_size = origin.size();
+		for (int i = 0; i < this->size(); i++)
+			this->_arr[i] = origin._arr[i];
+		return (*this);
+	};
+
+	// Getter - Setter
+	int     size(void) const { return (this->_size); }
+
+	T &operator[](const unsigned int idx) {
+		if (idx >= 0 && idx < _size) { return _arr[idx]; }
+		throw OutOfLimit();
 	}
 
 	class OutOfLimit : public std::exception {
@@ -70,24 +156,6 @@ public:
 			return (RED"Out of limits"RESET);
 		}
 	};
-
-	T &operator[](const unsigned int idx) {
-		if (idx >= 0 && idx < _size) { return _arr[idx]; }
-		throw OutOfLimit();
-	}
-
-	T *get_array() const;
-
-	size_t size() const ;
 };
-
-template <typename T>
-size_t Array<T>::size() const { return _size; }
-
-template <typename T>
-T *Array<T>::get_array() const {
-	return _arr;
-}
-
 
 #endif //EX02_ARRAY_HPP
--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -6,29 +6,66 @@
 #define MAX_VAL 750
 int main(int, char**)
 {
-//	Array<int> a(15);
-////	delete[] a;
-//	srand(time(NULL));
+//	int *z;
+//	z = new int[0];
+//	std::cout << z << std::endl;
+//	delete[] z;
+//	return 0;
+
+//	int * aa = new int();
+//	std::cout << *aa << std::endl;
+
+	Array<int> a(20);
+////	Array<int> b(10);
+////
+////	b = a;
+//
+////	Array<int> c = a;
+//	Array<int> b(a);
+//	Array<int> x;
+//	Array<int> z = a;
+//	x = a;
+
+//	delete[] a;
 //	Array<int> b(1);
 //	b[0] = 1234;
-//	for (int i =0; i < 15; ++i) {
-//		const int value = rand();
-//		a[i] = value;
+//	for (int i =0; i < 1; ++i) {
+//		a[i] = 42069;
 //		std::cout << a[i] << std::endl;
 //	}
-//	std::cout << "a[14] is " << a[14] << std::endl;
-//
-//	Array<int> c;
-//	Array<std::string> s(1);
-//	s[0] = "hello";
-//	std::cout << s[0] << std::endl;
-//
-//	std::cout << "b before =" << b[0] << std::endl;
-//	b = a;
-//	std::cout << "b after =" << b[0] << "b size " << b.size() << std::endl << std::endl;
-//
-//	Array<int> cpa(b);
-//	std::cout << "cpa is " << cpa[0] << "cpa size " << cpa.size() << std::endl;
+//	std::cout << "a[0] is " << a[0] << std::endl;
+
+//	----thats ok
+	Array<int> c;
+	c = a;
+	std::cout << "c.size is " << c.size() << "c.arr is " << c.size()<< std::endl;
+//	------thats not
+	Array<int> b = a;
+	std::cout << "b.size is " << b.size() << "b.arr[0] is ";
+	std::cout << b[0] << std::endl;
+
+//	Array<int> c = b;
+	Array<int> d;
+	d = a;
+	std::cout << "c.size is " << c.size() << "c.arr[0] is ";
+	std::cout << c[0] << std::endl;
+
+	c = a;
+	b = a;
+	Array<int> e;
+	d = a;
+	e = d;
+
+	Array<std::string> s(1);
+	s[0] = "hello";
+	std::cout << s[0] << std::endl;
+
+	std::cout << "b before =" << b[0] << std::endl;
+	b = a;
+	std::cout << "b after =" << b[0] << "b size " << b.size() << std::endl << std::endl;
+
+	Array<int> cpa(b);
+	std::cout << "cpa is " << cpa[0] << "cpa size " << cpa.size() << std::endl;
 //	//-----------------------------------intra ----------------------
     Array<int> numbers(MAX_VAL);
     int* mirror = new int[MAX_VAL];
